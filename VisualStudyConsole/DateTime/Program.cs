a8175c2eec0339e99640b973890bcc99a9889854
unknown <srang9503@gmail.com>
2022-05-19 21:27:12 +0900

    none

---
+++ VisualStudyConsole/DateTime/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeExample
+{
+
+    // 구조체 날짜 관련 Built In 
+    // 날짜 DateTime 구조체, TimeSpan 구조체 사용
+
+
+
+    class DateTimeExample
+    {
+        static void Main(string[] args)
+        {
+            // [1]  현재 시간 출력 : DateTime 구조체 (System.DateTime)
+            Console.WriteLine(DateTime.Now);
+
+            // [2] 년, 월, 일, 시, 분, 초 출력
+            Console.WriteLine(DateTime.Now.Year);
+            Console.WriteLine(DateTime.Now.Month);
+            Console.WriteLine(DateTime.Now.Day);
+            Console.WriteLine(DateTime.Now.Hour);
+            Console.WriteLine(DateTime.Now.Minute);
+            Console.WriteLine(DateTime.Now.Second);
+            Console.WriteLine(DateTime.Now.Millisecond);
+
+            // [3] DateTime 형식의 변수 선언
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now.ToLongDateString());
+
+            // [4] 시간차 구하기 : TimeSpan 구조체
+            TimeSpan dday 
+
+
+            // Console.WriteLine(new GetDataTimeFromHour().GetDateTimeFromYearlyHourNumber(25));
+        }
+
+
+
+
+    }
+
+
+    // 1년 8760시간
+    // 1 ~ 8760
+    // 1 => 2018년 1월 1일 0시
+    // 8760 => 2018년 12월 31일 23시
+
+    // f(x) => 0000년 00월 00일 00시
+    class GetDataTimeFromHour
+    {
+        public DateTime GetDateTimeFromYearlyHourNumber(int num)
+        {
+            return (new DateTime(2022, 1, 1, 0, 0, 0)).AddHours(--num);
+        }
+    }
+}
--- VisualStudyConsole/VisualStudyConsole/Program.cs
+++ VisualStudyConsole/VisualStudyConsole/Program.cs
@@ -6,10 +6,43 @@ using System.Threading.Tasks;
 
 namespace VisualStudyConsole
 {
-    class Program
+    struct Score
+    {
+        public string Name;
+        public int Kor;
+        public int Eng;
+        public int Total;
+        public double Avg;
+        public int Graph;
+    }
+
+    class struct_graph
     {
         static void Main(string[] args)
         {
+            Score[] scores = new Score[3];
+
+            scores[0].Name = "홍길동"; scores[0].Kor = 100; scores[0].Eng = 90;
+            scores[1].Name = "백두산"; scores[1].Kor = 90; scores[1].Eng = 50;
+            scores[2].Name = "임꺽정"; scores[2].Kor = 100; scores[2].Eng = 80;
+            
+            for (int i = 0; i < scores.Length; i++)
+            {
+                scores[i].Total = scores[i].Kor + scores[i].Eng;
+                scores[i].Avg = (double)scores[i].Total / 2;
+                scores[i].Graph = (int)scores[i].Avg / 5;
+            }
+
+            Console.WriteLine("이름\t 총점\t 평균\t 막대 그래프");
+            for (int i = 0; i < scores.Length; i++)
+            {
+                Console.Write("{0}\t {1}\t {2}\t", scores[i].Name, scores[i].Total, scores[i].Avg);
+                for(int j = 0; j<scores[i].Graph; j++)
+                {
+                    Console.Write("■");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
