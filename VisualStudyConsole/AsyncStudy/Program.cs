59afce85b6c245a48eeb6159d265461408f4770c
unknown <srang9503@gmail.com>
2022-05-30 23:53:16 +0900

    220530 review

---
+++ VisualStudyConsole/AsyncStudy/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 비동기 프로그래밍 : 한 번에 여러 개의 작업을 수행할 수 있도록 도와주는 기능이다.
+
+namespace AsyncStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
