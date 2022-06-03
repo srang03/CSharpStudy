7d7ecc9c05478cc94a206ac4726d216a2551261e
srang03 <srang9503@gmail.com>
2022-06-03 13:53:17 +0900

    220603 XML and JSON

---
+++ VisualStudyConsole/JSONDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONDemo
+{
+    public class Shirt
+    {
+        public string Name { get; set; }
+        public DateTime Created { get; set; }
+        public List<string> Sizes { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Shirt shirt = new Shirt()
+            {
+                Name = "Hi",
+                Created = DateTime.Now,
+                Sizes = new List<string>() { "Small", "medium", "Large" }
+            };
+
+            string json = JsonConvert.SerializeObject(shirt, Formatting.Indented);
+            Console.WriteLine(json);
+
+            var shrit_value = JsonConvert.DeserializeObject<Shirt>(json);
+            Console.WriteLine($"{shrit_value.Name}");
+
+        }
+    }
+}
--- VisualStudyConsole/XMLDemo/Program.cs
+++ VisualStudyConsole/XMLDemo/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace XMLDemo
 {
@@ -10,6 +11,21 @@ namespace XMLDemo
     {
         static void Main(string[] args)
         {
+            // [1] XML 요소 생성
+            XElement category = new XElement("MENUS",
+                new XElement("Menu", "책"),
+                new XElement("Menu", "강의"),
+                new XElement("Menu", "컴퓨터")
+            );
+
+            Console.WriteLine(category);
+
+            // [2] XML 요소 가공
+            XElement newCategory = new XElement("MENUS",
+                from node in category.Elements() where node.Value.ToString().Length > 2 select node
+            );
+
+            Console.WriteLine(newCategory);
         }
     }
 }
