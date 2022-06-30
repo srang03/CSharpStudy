f7af13934dd3d02e18e34a8815a8ce08309f5f61
srang03 <srang9503@gmail.com>
2022-06-30 14:25:35 +0900

    220630 Thread Review

---
+++ VisualStudyConsole/ThreadReview1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadReview1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"[T] : {Thread.CurrentThread.ManagedThreadId} Start");
+            Thread t1 = new Thread(print);
+            t1.Start();
+            Console.WriteLine($"[T] : {Thread.CurrentThread.ManagedThreadId} End");
+        }
+
+        static void print()
+        {
+            Console.WriteLine($"[T] : {Thread.CurrentThread.ManagedThreadId} Start");
+            Thread.Sleep(2000);
+            Console.WriteLine($"[T] : {Thread.CurrentThread.ManagedThreadId} End");
+            return;
+        }
+    }
+}
---
+++ VisualStudyConsole/ThreadReview2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadReview2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(Ide);
+            Thread t2 = new Thread(Sql);
+            Thread t3 = new Thread(Win)
+            {
+               
+            };
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            
+        }
+        private static void Ide()
+        {
+            Console.WriteLine($"T{Thread.CurrentThread.ManagedThreadId} : Ide tarting");
+            Thread.Sleep(2000);
+            Console.WriteLine($"T{Thread.CurrentThread.ManagedThreadId} : Ide End");
+        }
+
+        private static void Sql()
+        {
+            Console.WriteLine($"T{Thread.CurrentThread.ManagedThreadId} : Sql Starting");
+            Thread.Sleep(2000);
+            Console.WriteLine($"T{Thread.CurrentThread.ManagedThreadId} : Sql End");
+        }
+
+        private static void Win()
+        {
+            Console.WriteLine($"T{Thread.CurrentThread.ManagedThreadId} : Win Starting");
+            Thread.Sleep(2000);
+            Console.WriteLine($"T{Thread.CurrentThread.ManagedThreadId} : Win End");
+        }
+    }
+}
