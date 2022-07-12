7e6f48e72796de06a16a7d4870d46316727f8699
unknown <srang9503@gmail.com>
2022-07-12 22:33:54 +0900

    220712 extension method

---
+++ VisualStudyConsole/Extension/ExtensionMethod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension
+{
+    // 확장 메서드의 규칙 1. static 클래스를 생성한다.
+    //                    2. static 함수의 첫 번째 인자로 확장 메서드의 타입, 두 번째 인자부터 매개변수로 사용한다.
+    //                    3. 동일한 namespace 안에 위치해야한다.
+    public static class ExtensionMethod
+    {
+        public static string ToPlus(this string a, string b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ VisualStudyConsole/Extension/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Hello";
+            greeting.ToPlus("C#");
+        }
+    }
+}
