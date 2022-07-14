ca85068ac2359171ce181228d852f32ab72581c5
srang03 <srang9503@gmail.com>
2022-07-14 11:47:21 +0900

    220714 network review

---
+++ VisualStudyConsole/ModelMemoConsoleApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using ModelMemo;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelMemoConsoleApp
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            
+            string url = "https://localhost:44343/api/ModelMemo";
+            using(var client = new HttpClient())
+            {
+                // 데이터 전송 (저장)
+                    // #1 Data 생성
+                var json = JsonConvert.SerializeObject(new Model()
+                {
+                    Title = "Win Console",
+                    IsDone = true
+                });
+                    // #2 Data Body 생성
+                var post = new StringContent(json, Encoding.UTF8, "application/json");
+                    // #3 전송
+                await client.PostAsync(url, post);
+
+                // 데이터 수신 (출력)
+                var response = await client.GetAsync(url);
+                var result = await response.Content.ReadAsStringAsync();
+                var todos = JsonConvert.DeserializeObject<List<Model>>(result);
+
+                foreach (var item in todos)
+                {
+                    Console.WriteLine($"[{item.Id}] {item.Title} : ${item.IsDone}");
+                }
+                
+            }
+
+
+
+        }
+    }
+}
+
---
+++ VisualStudyConsole/ModelMemoNetworksWebApi/ModelMemoController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelMemo;
+
+namespace ModelMemoNetworksWebApi
+{
+    
+    [Route("api/[Controller]")]
+    // Route Attribute (ModelMemo 접두사 사용하고 Controller 이름 접미사 제거)
+    public class ModelMemoController: ControllerBase
+    {
+
+        private IRepositoryService _repository;
+        public ModelMemoController()
+        {
+             _repository = new RepositoryServiceJson();
+
+        }
+        // Http 서비스를 통해서 데이터를 출력해서 전달하고 싶다.
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            // return Content("안녕하세요")
+            return Ok(_repository.ListModel());
+        }
+
+        // Http 서비스를 통해서 전달되는 것을 받고 싶다.
+        // Body에 담아서 보내기 위한 Post 방식
+        [HttpPost]
+        public IActionResult Add([FromBody]Model model)
+        {
+            _repository.AddMemo(model);
+            return Ok(model);
+        }
+    }
+}
