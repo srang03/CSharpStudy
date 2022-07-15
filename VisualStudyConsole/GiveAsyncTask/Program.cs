07d3baab7803e349c9e94c9664211c0cd4ba6a33
srang03 <srang9503@gmail.com>
2022-07-15 14:42:20 +0900

    2201715 async console

---
+++ VisualStudyConsole/GiveAsyncTask/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using TaskRunMethod;
+
+namespace GiveAsyncTask
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("실행 방식 선택");
+            Console.WriteLine("[0]동기  [1]await 비동기  [2]비동기  [3]WhenAll  [4]WhenAny");
+            string key = Console.ReadLine();
+            Console.WriteLine("메인 함수 시작");
+            Cooking cooking = new Cooking();
+            switch (key)
+            {
+                
+                case "0":
+                    {
+                        Stopwatch sw = new Stopwatch();
+                        sw.Start();
+                        cooking.MakeRice();
+                        cooking.MakeSoup();
+                        cooking.MakeEgg();
+                        sw.Stop();
+                        Console.WriteLine(sw.ElapsedMilliseconds);
+                    }
+                break;
+
+                case "1":
+                    {
+                        Stopwatch sw = new Stopwatch();
+                        sw.Start();
+                        await cooking.MakeRiceAsync();
+                        await cooking.MakeSoupAsync();
+                        await cooking.MakeEggeAsync();
+                        sw.Stop();
+                        Console.WriteLine(sw.ElapsedMilliseconds);
+                        break;
+                    }
+                case "2":
+                    {
+                        Stopwatch sw = new Stopwatch();
+                        sw.Start();
+                        Task<Rice> riceTask = cooking.MakeRiceAsync();
+                        Task<Soup> soupTask = cooking.MakeSoupAsync();
+                        Task<Egg> eggTask = cooking.MakeEggeAsync();
+
+                        Rice rice = await riceTask;
+                        Soup soup = await soupTask;
+                        Egg egg = await eggTask;
+
+                        sw.Stop();
+                        Console.WriteLine(sw.ElapsedMilliseconds);
+                        break;
+                    }
+
+                case "3":
+                    {
+                        Stopwatch sw = new Stopwatch();
+                        sw.Start();
+
+                        Task<Rice> riceTask = cooking.MakeRiceAsync();
+                        Task<Soup> soupTask = cooking.MakeSoupAsync();
+                        Task<Egg> eggTask = cooking.MakeEggeAsync();
+
+                        await Task.WhenAll(riceTask, soupTask, eggTask);
+                        sw.Stop();
+                        Console.WriteLine(sw.ElapsedMilliseconds);
+                        break;
+                    }
+                default:
+                    break;
+                    
+            }
+            Console.WriteLine("메인함수 종료");
+            
+
+        }
+    }
+}
---
+++ VisualStudyConsole/TaskRunMethod/Cooking.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskRunMethod
+{
+    public class Cooking
+    {
+        public Rice MakeRice()
+        {
+            Console.WriteLine("Cooking for Rice");
+            Thread.Sleep(2000);
+            Console.WriteLine("Rice End");
+            return new Rice();
+        }
+
+        public Task<Rice> MakeRiceAsync()
+        {
+            return Task.Run(() =>
+            {
+                Console.WriteLine("Cooking for Rice");
+                Thread.Sleep(3000);
+                Console.WriteLine("Rice End");
+                return new Rice();
+            });
+        }
+
+        public Soup MakeSoup()
+        {
+            Console.WriteLine("Cooking for Soup");
+            Thread.Sleep(1000);
+            Console.WriteLine("Soup End");
+            return new Soup();
+        }
+
+        public Task<Soup> MakeSoupAsync()
+        {
+            return Task.Run(() =>
+            {
+                Console.WriteLine("Cooking for Soup");
+                Thread.Sleep(2000);
+                Console.WriteLine("Soup End");
+                return new Soup();
+            });
+        }
+
+        public Egg MakeEgg()
+        {
+            Console.WriteLine("Cooking for Egg");
+            Thread.Sleep(2000);
+            Console.WriteLine("Egg End");
+            return new Egg();
+        }
+
+        public Task<Egg> MakeEggeAsync()
+        { 
+             Console.WriteLine("Cooking for Egg");
+            long sum = 0;
+            for (int i = 0; i < 1_000_000_000; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine("Egg End");
+            return Task.FromResult(new Egg());
+        }
+    }
+}
